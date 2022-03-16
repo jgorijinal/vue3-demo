9f3290e6a6353c2dfc0f9488b40f2e0c402f55af
jgoriginal <932274204@qq.com>
2022-03-16 14:16:18 +0800

    topNav的样式更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
 <div class="topNav">
-  <div class="logo">logo</div>
-  <div class="menu">menu</div>
+  <div class="logo">LOGO</div>
+  <ul class="menu">
+    <li>菜单1</li>
+    <li>菜单2</li>
+  </ul>
 </div>
   <div class="banner">
     <h1>DNA UI</h1>
@@ -18,5 +21,23 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-
+.topNav {
+  background: pink;
+  display: flex;
+  padding: 16px;
+  color: #213547;
+  border-bottom: 1px solid #e7e7e8;
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+  }
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    > li {
+      margin: 0 1em;
+    }
+  }
+}
 </style>
