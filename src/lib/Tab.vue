e0621928bce81b278c9510b74a3a3d54cf52c39b
jgoriginal <932274204@qq.com>
2022-03-17 23:10:34 +0800

    检查子组件的类型是不是Tab

--- src/lib/Tab.vue
+++ src/lib/Tab.vue
@@ -1,5 +1,9 @@
 <template>
-  <div> Tab </div>
+  <div class="gulu-tab-title">{{title}}</div>
+  <div class="gulu-tab-content">
+    <slot></slot>
+  </div>
+
 </template>
 <script lang="ts">
 export default  {
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,10 +1,19 @@
 <template>
-  <div> Tabs </div>
+  <component :is="defaults[0]"></component>
+  <component :is="defaults[1]"></component>
 </template>
 <script lang="ts">
 import Tab from './Tab.vue'
 export default  {
-
+  setup(props , context){
+    const defaults = context.slots.default()
+    defaults.forEach(tab => {
+      if(tab.type !== Tab) {
+        throw new Error ('Tabs的子标签必须是Tab')
+      }
+    })
+    return {defaults}
+  }
 }
 </script>
 <style lang="scss">
