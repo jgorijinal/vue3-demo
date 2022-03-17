0b99468f33d1168c3ac41c1bb0418ab235cd4d06
jgoriginal <932274204@qq.com>
2022-03-17 20:06:03 +0800

    支持visible属性

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,10 +1,20 @@
 <template>
-  <Dialog />
+  <Button theme="primary" @click="toggle"> 打开Dialog</Button>
+  <Dialog  v-model:visible="visible" />
 </template>
 <script lang="ts">
-import Dialog from '../lib/DIalog.vue'
+import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue'
+import {ref} from 'vue';
 export default {
-  components:{Dialog}
+  components:{Dialog , Button},
+  setup(){
+    const visible = ref(false)
+    const toggle = ()=>{
+      visible.value  = !visible.value
+    }
+    return {toggle , visible}
+  }
 }
 </script>
 <style>
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,44 +1,62 @@
 <template>
-  <div class="gulu-dialog-overlay"></div>
-  <div class="gulu-dialog-wrapper">
-    <div class="gulu-dialog">
-      <header>标题 <span class="gulu-dialog-close"></span></header>
-      <main>
-        <p>第一行字</p>
-        <p>第二行字</p>
-      </main>
-      <footer>
-        <Button theme="primary">确定</Button>
-        <Button>取消</Button>
-      </footer>
+  <Transition>
+    <div v-if="visible">
+      <div class="gulu-dialog-overlay"></div>
+      <div class="gulu-dialog-wrapper">
+        <div class="gulu-dialog">
+          <header>标题 <span class="gulu-dialog-close" @click="close"></span></header>
+          <main>
+            <p>第一行字</p>
+            <p>第二行字</p>
+          </main>
+          <footer>
+            <Button theme="primary">确定</Button>
+            <Button>取消</Button>
+          </footer>
+        </div>
+      </div>
     </div>
-  </div>
+  </Transition>
 </template>
 <script lang="ts">
 import Button from './Button.vue';
 
 export default {
-  components: {Button}
+  components: {Button},
+  props: {
+    visible: {
+      type: Boolean,
+    }
+  },
+  setup(props,context){
+    const close = ()=>{
+      context.emit('update:visible' , !props.visible)
+    }
+    return {close}
+  }
 };
 </script>
 <style lang="scss">
 $radius: 4px;
 $border-color: #d9d9d9;
+
 .gulu-dialog {
   background: white;
   border-radius: $radius;
   box-shadow: 0 0 3px fade_out(black, 0.5);
   min-width: 20em;
   max-width: 90%;
+
   &-overlay {
     position: fixed;
-    top: 0;
+    top: 58px;
     left: 0;
     width: 100%;
     height: 100%;
     background: fade_out(black, 0.5);
     z-index: 10;
   }
+
   &-wrapper {
     position: fixed;
     left: 50%;
@@ -46,7 +64,8 @@ $border-color: #d9d9d9;
     transform: translate(-50%, -50%);
     z-index: 11;
   }
-  >header {
+
+  > header {
     padding: 12px 16px;
     border-bottom: 1px solid $border-color;
     display: flex;
@@ -54,20 +73,24 @@ $border-color: #d9d9d9;
     justify-content: space-between;
     font-size: 20px;
   }
-  >main {
+
+  > main {
     padding: 12px 16px;
   }
-  >footer {
+
+  > footer {
     border-top: 1px solid $border-color;
     padding: 12px 16px;
     text-align: right;
   }
+
   &-close {
     position: relative;
     display: inline-block;
     width: 16px;
     height: 16px;
     cursor: pointer;
+
     &::before,
     &::after {
       content: '';
@@ -78,12 +101,22 @@ $border-color: #d9d9d9;
       top: 50%;
       left: 50%;
     }
+
     &::before {
       transform: translate(-50%, -50%) rotate(-45deg);
     }
+
     &::after {
       transform: translate(-50%, -50%) rotate(45deg);
     }
   }
+}
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.25s ease;
+}
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
 }
 </style>
