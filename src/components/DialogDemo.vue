ba51ef70f1b46fbf8c6fd25384112cf3af44b5b9
jgoriginal <932274204@qq.com>
2022-03-17 21:59:39 +0800

    已一句话打开Dialog共风能实现

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -4,10 +4,16 @@
     <template v-slot:title>这里是标题</template>
     <template v-slot:content>这里是内容哦</template>
   </Dialog>
+
+  <h1>实例2</h1>
+  <Button @click="onClick"> 一句话打开Dialog</Button>
 </template>
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue'
 import Button from '../lib/Button.vue'
+
+import {openDialog} from '../lib/openDialog';
+
 import {ref} from 'vue';
 export default {
   components:{Dialog , Button},
@@ -22,7 +28,16 @@ export default {
     const f2 = ()=>{
       console.log(2)
     }
-    return {toggle , visible , f1 ,f2}
+    const onClick = ()=>{
+      openDialog({
+        title : '标题',
+        content: '内容',
+        ok:()=>{console.log('ok')},
+        cancel:()=>{console.log('cancel')},
+      })
+    }
+
+    return {toggle , visible , f1 ,f2 , onClick}
   }
 }
 </script>
